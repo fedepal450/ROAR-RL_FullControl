ae314387625122e829dc2415ba268f8c71785031
Franco Leonardo Huang <francohuang945@hotmail.com>
2021-11-29 22:47:07 -0800

    change the range back

--- ROAR_gym/ROAR_Gym/envs/e2eModel_roar_env.py
+++ ROAR_gym/ROAR_Gym/envs/e2eModel_roar_env.py
@@ -78,8 +78,8 @@ class ROARppoEnvE2E(ROAREnv):
         rewards = []
 
         for i in range(FRAME_STACK):
-            throttle=np.power(action[i*3+0],0.1)
-            steering=np.sign(action[i*3+1])*np.max([np.power(action[i*3+1],10)-0.5,0])
+            throttle=np.min([np.power(action[i*3+0],0.1)*2,1])
+            steering=np.sign(action[i*3+1])*np.max([np.power(action[i*3+1],10)*4-3,0])
             braking=np.max([np.square(action[i*3+2])-0.9,0])
             self.agent.kwargs["control"] = VehicleControl(throttle=throttle,
                                                           steering=steering,
