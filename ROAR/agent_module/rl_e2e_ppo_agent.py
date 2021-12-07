78cfb689769e9b3ed30841d4e3b2712c0cfdfed8
Franco Leonardo Huang <francohuang945@hotmail.com>
2021-12-07 13:46:53 -0800

    Revert "waylines change in agent"
    This reverts commit ca7ade081b617ceb681608124575f39b06bd004b.

--- ROAR/agent_module/rl_e2e_ppo_agent.py
+++ ROAR/agent_module/rl_e2e_ppo_agent.py
@@ -15,7 +15,6 @@ from ROAR.utilities_module.data_structures_models import Transform, Location
 import cv2
 from typing import Optional
 import scipy.stats
-from collections import deque
 
 class RLe2ePPOAgent(Agent):
     def __init__(self, vehicle: Vehicle, agent_settings: AgentConfig, **kwargs):
@@ -36,16 +35,19 @@ class RLe2ePPOAgent(Agent):
         # the part about visualization
         self.occupancy_map = OccupancyGridMap(agent=self, threaded=True)
 
-        occ_file_path = Path("../ROAR_Sim/data/berkeley_minor_global_occu_map.npy")
+        occ_file_path = Path("../ROAR_Sim/data/berkeley_minor_cleaned_global_occu_map.npy")
         self.occupancy_map.load_from_file(occ_file_path)
 
         self.plan_lst = list(self.mission_planner.produce_single_lap_mission_plan())
 
         self.kwargs = kwargs
-        self.interval = self.kwargs.get('interval', 5)
+        self.interval = self.kwargs.get('interval', 50)
         self.look_back = self.kwargs.get('look_back', 5)
         self.look_back_max = self.kwargs.get('look_back_max', 10)
         self.thres = self.kwargs.get('thres', 1e-3)
+
+        middle=scipy.stats.norm(20//2, 20//3).pdf(20//2)
+        self.bbox_reward_list=[scipy.stats.norm(20//2, 20//3).pdf(i)/middle*0.5 for i in  range(20)]
 
         self.int_counter = 0
         self.cross_reward=0
@@ -53,21 +55,27 @@ class RLe2ePPOAgent(Agent):
         self.finished = False
         self.curr_dist_to_strip = 0
         self.bbox: Optional[LineBBox] = None
-        self.bbox_list = []# list of bbox
-        self.frame_queue = deque(maxlen=4)
-        #self._get_next_bbox()
-        self._get_all_bbox()
+        # self.bbox_list = []# list of bbox
+        self._get_next_bbox()
 
-    def run_step(self, sensors_data: ViveTrackerData, vehicle: Vehicle) -> VehicleControl:
-        super(RLe2ePPOAgent, self).run_step(sensors_data, vehicle)
+    def reset(self,vehicle: Vehicle):
+        self.vehicle=vehicle
+        self.int_counter = 0
+        self.cross_reward=0
+        self.counter = 0
+        self.finished = False
+        self.curr_dist_to_strip = 0
+        self.bbox: Optional[LineBBox] = None
+        # self.bbox_list = []# list of bbox
+        self._get_next_bbox()
+
+    def run_step(self,vehicle: Vehicle) -> VehicleControl:
+        # super(RLe2ePPOAgent, self).run_step(sensors_data, vehicle)
         #print(self.vehicle.transform)
         #self.local_planner.run_in_series()#TO REMOVE
 
+        self.vehicle = vehicle
         self.curr_dist_to_strip = self.bbox_step()
-        if self.kwargs.get("control") is None:
-            return VehicleControl()
-        else:
-            return self.kwargs.get("control")
 
     def bbox_step(self):
         """
@@ -77,6 +85,7 @@ class RLe2ePPOAgent(Agent):
         return:
         crossed: a boolean value indicating whether a new strip is reached
         dist (optional): distance to the strip, value no specific meaning
+        """
         self.counter += 1
         if not self.finished:
             while(True):
@@ -90,50 +99,6 @@ class RLe2ePPOAgent(Agent):
 
             return dist
         return False, 0.0
-        """
-        if self.counter < len(self.bbox_list):
-            while(True):
-                crossed, dist = self.bbox_list[self.counter].has_crossed(self.vehicle.transform)
-                if crossed:
-                    self.counter += 1
-                    self.cross_reward+=crossed
-                    if len(self.frame_queue) < 4:
-                        self.frame_queue.append(self.counter)
-                    else:
-                        self.frame_queue.popleft()
-                        self.frame_queue.append(self.counter)
-                else:
-                    self.frame_queue.append(-1)
-                    break
-            return dist
-        return False, 0.0
-
-    def _get_all_bbox(self):
-        local_int_counter = 0
-        curr_lb = self.look_back
-        curr_idx = local_int_counter * self.interval
-        while curr_idx + curr_lb < len(self.plan_lst):
-            if curr_lb > self.look_back_max:
-                local_int_counter += 1
-                curr_lb = self.look_back
-                curr_idx = local_int_counter * self.interval
-                continue
-
-            t1 = self.plan_lst[curr_idx]
-            t2 = self.plan_lst[curr_idx + curr_lb]
-
-            dx = t2.location.x - t1.location.x
-            dz = t2.location.z - t1.location.z
-            if abs(dx) < self.thres and abs(dz) < self.thres:
-                curr_lb += 1
-            else:
-                self.bbox_list.append(LineBBox(t1, t2))
-                local_int_counter += 1
-                curr_lb = self.look_back
-                curr_idx = local_int_counter * self.interval
-        # no next bbox
-        print("finished all the iterations!")
-        #self.finished = True
 
     def _get_next_bbox(self):
         # make sure no index out of bound error
@@ -154,16 +119,47 @@ class RLe2ePPOAgent(Agent):
             if abs(dx) < self.thres and abs(dz) < self.thres:
                 curr_lb += 1
             else:
-                self.bbox = LineBBox(t1, t2)
+                self.bbox = LineBBox(t1, t2,self.bbox_reward_list)
                 return
         # no next bbox
         print("finished all the iterations!")
         self.finished = True
 
+    # def _get_bbox_list(self, num):
+    #     # make sure no index out of bound error
+    #     self.bbox_list = []#clear bbox_list
+    #     curr_lb = self.look_back
+    #     curr_idx = self.int_counter * self.interval
+    #     while curr_idx + curr_lb < len(self.plan_lst):
+    #         if curr_lb > self.look_back_max:
+    #             self.int_counter += 1
+    #             curr_lb = self.look_back
+    #             curr_idx = self.int_counter * self.interval
+    #             continue
+    #
+    #         t1 = self.plan_lst[curr_idx]
+    #         t2 = self.plan_lst[curr_idx + curr_lb]
+    #
+    #         dx = t2.location.x - t1.location.x
+    #         dz = t2.location.z - t1.location.z
+    #         if abs(dx) < self.thres and abs(dz) < self.thres:
+    #             curr_lb += 1
+    #         else:
+    #             if num > 0:
+    #                 num -= 1
+    #                 self.bbox_list.append(LineBBox(t1, t2))
+    #                 curr_lb = self.look_back #update curr_lb
+    #                 curr_idx = self.int_counter * self.interval
+    #             else:
+    #                 return
+    #
+    #     # no next bbox
+    #     print("finished all the iterations!")
+    #     self.finished = True
 
 
 class LineBBox(object):
-    def __init__(self, transform1: Transform, transform2: Transform) -> None:
+    def __init__(self, transform1: Transform, transform2: Transform,bbox_reward_list) -> None:
         self.x1, self.z1 = transform1.location.x, transform1.location.z
         self.x2, self.z2 = transform2.location.x, transform2.location.z
         #print(self.x2, self.z2)
@@ -173,6 +169,9 @@ class LineBBox(object):
         self.dis = self._construct_dis()
         self.strip_list = None
         self.size=20
+        self.bbox_reward_list=bbox_reward_list
+        self.strip_list = None
+        self.generate_visualize_locs(20)
 
         if self.eq(self.x1, self.z1) > 0:
             self.pos_true = False
@@ -237,7 +236,7 @@ class LineBBox(object):
         middle=scipy.stats.norm(self.size//2, self.size//2).pdf(self.size//2)
         return (scipy.stats.norm(self.size//2, self.size//2).pdf(self.size//2-self.dis(x, z))/middle if crossed else 0, dist)
 
-    def get_visualize_locs(self, size=10):
+    def generate_visualize_locs(self, size=10):
         if self.strip_list is not None:
             return self.strip_list
 
@@ -258,19 +257,28 @@ class LineBBox(object):
         self.strip_list = []
         for i in range(len(xs)):
             self.strip_list.append(Location(x=xs[i], y=0, z=zs[i]))
+
+    def get_visualize_locs(self):
         return self.strip_list
 
-    def get_value(self,size=10):
-        middle=scipy.stats.norm(size//2, size//2).pdf(size//2)
-        return [scipy.stats.norm(size//2, size//2).pdf(i)/middle*0.5 for i in  range(size)]
 
-    def get_directional_velocity(self,x,z):
+    def get_value(self):
+        return self.bbox_reward_list
+
+    def get_directional_velocity(self,x,y):
         dz, dx = self.z2 - self.z1, self.x2 - self.x1
         dx,dz=[dx,dz]/np.linalg.norm([dx,dz])
-        return dx*x+dz*z
+        return dx*x+dz*y
 
-    def to_array(self):
+    def to_array(self,x,z):
         dz, dx = self.z2 - self.z1, self.x2 - self.x1
-        slope_ = dz / (dx+1e-30)
-        angle=np.arctan(slope_)/np.pi*2
-        return np.array([self.x2 , self.z2,angle])
+        angle1=np.arctan2(-dx,-dz)/np.pi
+
+        dz, dx = self.z2 - z, self.x2 - x
+        angle2=np.arctan2(-dx,-dz)/np.pi
+        return np.array([dx, dz, np.sqrt(np.square(dz)+np.square(dx)),angle1,angle2])
+
+    def get_yaw(self):
+        dz, dx = self.z2 - self.z1, self.x2 - self.x1
+        angle=np.arctan2(-dx,-dz)/np.pi*180
+        return angle
